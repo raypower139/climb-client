859ff4720de8373fc81a9c6ad630615ce5364b0d
Ray Power <39620873+raypower139@users.noreply.github.com>
2019-04-26 20:33:40 +0100

    Routes Added

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,26 @@
+import { RouterConfiguration, Router } from 'aurelia-router';
+import { PLATFORM } from 'aurelia-pal';
+
 export class App {
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router) {
+    config.map([
+      {
+        route: ['', 'climb'],
+        name: 'Climb',
+        moduleId: PLATFORM.moduleName('views/climbs'),
+        nav: true,
+        title: 'Climb'
+      },
+      {
+        route: 'categories',
+        name: 'categories',
+        moduleId: PLATFORM.moduleName('views/categories'),
+        nav: true,
+        title: 'Category'
+      }
+    ]);
+    this.router = router;
+  }
 }
